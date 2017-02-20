bb4c1a81f5e80f098952a67ef1b14c67fbb9498f
Juan E. Maya <jmayaalv@gmail.com>
2017-02-20 14:23:56 +0100

    Naked twins

--- solution.py
+++ solution.py
@@ -3,6 +3,18 @@ assignments = []
 rows = 'ABCDEFGHI'
 cols = '123456789'
 
+def display(values):
+    """
+    Display the values as a 2-D grid.
+    :param  values: The sudoku in dictionary form
+    """
+    width = 1 + max(len(values[s]) for s in boxes)
+    line = '+'.join(['-' * (width * 3)] * 3)
+    for r in rows:
+        print(''.join(values[r + c].center(width) + ('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+
 
 def assign_value(values, box, value):
     """
@@ -14,17 +26,15 @@ def assign_value(values, box, value):
         assignments.append(values.copy())
     return values
 
-
-def naked_twins(values):
-    """Eliminate values using the naked twins strategy.
-    :param: values: a dictionary of the form {'box_name': '123456789', ...}
-
-    :return:
-        the values dictionary with the naked twins eliminated from peers.
+def __remove_digit(values, box, digit):
     """
+    Removes a digt from a box
+    :param values: a grid in dict form
+    :param box: box where the values wants to be removed
+    :return: updated dict with the puzzle value
+    """
+    return  assign_value(values, box, values[box].replace(digit, ''))
 
-    # Find all instances of naked twins
-    # Eliminate the naked twins as possibilities for their peers
 
 
 def cross(a, b):
@@ -38,11 +48,44 @@ def cross(a, b):
 boxes = cross(rows, cols)
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
-square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
 unitlist = row_units + column_units + square_units
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
-peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+peers = dict((s, set(sum(units[s], [])) - set([s])) for s in boxes)
 
+
+def __find_naked_twins(values, unit):
+    """Find the naked twins in a unit
+        :param: values: a grid in dict form
+        :param: unit: a list of boxes
+
+        :return: list of boxes with their value appearing exactly in two boxes.
+    """
+    boxes_with_pair = [box for box in unit if len(values[box]) == 2]
+
+    twins = set()
+    for box in boxes_with_pair:
+        twins |= {peer for peer in boxes_with_pair if values[box] == values[peer] and box != peer}
+    return list(twins)
+
+
+def naked_twins(values):
+    """Eliminate values using the naked twins strategy.
+    :param: values: a dictionary of the form {'box_name': '123456789', ...}
+
+    :return:
+        the values dictionary with the naked twins eliminated from peers.
+    """
+    for unit in unitlist:
+        twins = __find_naked_twins(values, unit)
+        if len(twins) == 2:
+            print(twins)
+            twin_digits = values[twins[0]]
+            for box in unit:
+                if box not in twins:
+                    for digit in twin_digits:
+                        values = __remove_digit(values, box, digit)
+    return values
 
 
 def grid_values(grid):
@@ -57,17 +100,7 @@ def grid_values(grid):
     return dict(map(lambda t: (t[0], cols if t[1] == '.' else t[1]), zip(boxes, grid)))
 
 
-def display(values):
-    """
-    Display the values as a 2-D grid.
-    :param  values: The sudoku in dictionary form
-    """
-    width = 1 + max(len(values[s]) for s in boxes)
-    line = '+'.join(['-' * (width * 3)] * 3)
-    for r in rows:
-        print(''.join(values[r + c].center(width) + ('|' if c in '36' else '')
-                      for c in cols))
-        if r in 'CF': print(line)
+
 
 
 def eliminate(values):
@@ -80,7 +113,7 @@ def eliminate(values):
     for box in solved_values:
         digit = values[box]
         for peer in peers[box]:
-            values[peer] = values[peer].replace(digit, '')
+            values = __remove_digit(values, peer, digit)
     return values
 
 
@@ -97,9 +130,8 @@ def only_choice(values):
             for d in digits:
                 candidate = [box for box in unit if d in values[box]]
                 if len(candidate) == 1:
-                    values[candidate[0]] = d
+                    values = assign_value(values, candidate[0], d)
     return values
-
 
 
 def reduce_puzzle(values):
